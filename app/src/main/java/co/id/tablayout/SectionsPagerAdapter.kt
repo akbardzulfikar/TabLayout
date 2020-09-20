26fa19e82e4f24ca8399bda2eebe9ca220a6a8a7
laksamana.akbar <laksamana.guntur@mekar.id>
2020-09-20 16:29:02 +0700

    Initial commit

---
+++ app/src/main/java/co/id/tablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,35 @@
+package co.id.tablayout
+
+import android.content.Context
+import androidx.annotation.Nullable
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    @StringRes
+    private val TAB_TTILES = intArrayOf(
+        R.string.tab_text_1,
+        R.string.tab_text_2,
+        R.string.tab_text_3)
+
+    override fun getCount(): Int = 3
+
+    override fun getItem(position: Int): Fragment {
+//        var fragment: Fragment? = null
+//        when(position) {
+//            0 -> fragment = HomeFragment()
+//            1-> fragment = ProfileFragment()
+//        }
+//        return fragment as Fragment
+        val fragment = HomeFragment.newInstance(position + 1)
+        return fragment
+    }
+
+    @Nullable
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TTILES[position])
+    }
+}
